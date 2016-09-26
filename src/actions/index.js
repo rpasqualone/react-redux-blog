947e77550e6945ae8d9d88e4221faf427691dfde
richardp <richardp@eCreations.net>
2016-09-25 20:53:21 -0700

    Working Create Post form.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,13 +4,21 @@ const API_KEY = '?key=richard.pasqualone';
 const ROOT_URL = `http://reduxblog.herokuapp.com/api`;
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-
 export function fetchPosts() {
-	const url = `${ROOT_URL}/posts${API_KEY}`;
-	const request = axios.get(url);
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
 	return {
 		type: FETCH_POSTS,
+		payload: request
+	}
+}
+
+export const CREATE_POST = 'CREATE_POST';
+export function createPost(props) {
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+	return {
+		type: CREATE_POST,
 		payload: request
 	}
 }
--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 	render() {
 		const { fields: { title, categories, content }, handleSubmit } = this.props;
-		console.log(title);
 
 		return (
-			<form onsubmit={handleSubmit}>
+			<form onSubmit={handleSubmit(this.props.createPost)}>
 				<h3>Create A New Post</h3>
 				<div className="form-group">
 					<lable>Title</lable>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...title} />
 				</div>
 				<div className="form-group">
 					<lable>Categories</lable>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...categories} />
 				</div>
 				<div className="form-group">
 					<lable>Content</lable>
-					<textarea type="text" className="form-control" />
+					<textarea type="text" className="form-control" {...content} />
 				</div>
 				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
@@ -30,4 +30,4 @@ class PostsNew extends Component {
 export default reduxForm({
 	form: 'PostsNew',
 	fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
